709e33bc801d3d1a5b10098e58cff9858bb51f41
Stefan Foulis <stefan@foulis.ch>
2012-12-12 15:35:03 +0100

    adds minimal security check

--- djangocms_link/fields.py
+++ djangocms_link/fields.py
@@ -4,7 +4,19 @@ try:
     class PageSearchField(AutoModelSelect2Field):
         search_fields = ['title_set__title__icontains', 'title_set__menu_title__icontains', 'title_set__slug__icontains']
 
+        def security_check(self, request, *args, **kwargs):
+            user = request.user
+            if user and not user.is_anonymous() and user.is_staff:
+                return True
+            return False
+
     class UserSearchField(AutoModelSelect2Field):
         search_fields = ['username__icontains', 'firstname__icontains', 'lastname__icontains']
+
+        def security_check(self, request, *args, **kwargs):
+            user = request.user
+            if user and not user.is_anonymous() and user.is_staff:
+                return True
+            return False
 except ImportError:
     pass
