c52c78e2c21f78c7d41381c3def08789619dd5c6
Paulo <commonzenpython@gmail.com>
2014-09-12 00:39:25 -0400

    fixes issue with required page field

--- djangocms_link/fields.py
+++ djangocms_link/fields.py
@@ -1,42 +1,38 @@
 from django.conf import settings
+from django.core import validators
+
+
 if 'django_select2' in settings.INSTALLED_APPS:
-    try:
+    from django_select2.fields import AutoModelSelect2Field
 
-        from django_select2.fields import AutoModelSelect2Field
 
-        class PageSearchField(AutoModelSelect2Field):
-            empty_values = []
-            search_fields = ['title_set__title__icontains', 'title_set__menu_title__icontains', 'title_set__slug__icontains']
+    class PageSearchField(AutoModelSelect2Field):
+        search_fields = [
+            'title_set__title__icontains',
+            'title_set__menu_title__icontains',
+            'title_set__slug__icontains'
+        ]
 
-            def get_queryset(self):
-                queryset = super(PageSearchField, self).get_queryset()
-                return queryset.distinct()
+        def get_queryset(self):
+            queryset = super(PageSearchField, self).get_queryset()
+            return queryset.distinct()
 
-            def security_check(self, request, *args, **kwargs):
-                user = request.user
-                if user and not user.is_anonymous() and user.is_staff:
-                    return True
-                return False
+        def security_check(self, request, *args, **kwargs):
+            user = request.user
+            if user and not user.is_anonymous() and user.is_staff:
+                return True
+            return False
 
-            def prepare_value(self, value):
-                if not value:
-                    return None
-                return super(PageSearchField, self).prepare_value(value)
+    class UserSearchField(AutoModelSelect2Field):
+        search_fields = ['username__icontains', 'firstname__icontains', 'lastname__icontains']
 
-        class UserSearchField(AutoModelSelect2Field):
-            search_fields = ['username__icontains', 'firstname__icontains', 'lastname__icontains']
+        def security_check(self, request, *args, **kwargs):
+            user = request.user
+            if user and not user.is_anonymous() and user.is_staff:
+                return True
+            return False
 
-            def security_check(self, request, *args, **kwargs):
-                user = request.user
-                if user and not user.is_anonymous() and user.is_staff:
-                    return True
-                return False
-
-            def prepare_value(self, value):
-                if not value:
-                    return None
-                return super(PageSearchField, self).prepare_value(value)
-
-    except ImportError:
-        pass
-
+        def prepare_value(self, value):
+            if not value:
+                return None
+            return super(UserSearchField, self).prepare_value(value)
