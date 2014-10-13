1dd461737e9f1c45bca7f0514ec87e55a1c457a0
Jonas Obrist <ojiidotch@gmail.com>
2014-10-14 00:26:51 +0900

    made link plugin work with my sane-add-plugin branch on ojii/django-cms

--- djangocms_link/cms_plugins.py
+++ djangocms_link/cms_plugins.py
@@ -8,6 +8,7 @@ from cms.plugin_base import CMSPluginBase
 from djangocms_link.forms import LinkForm
 from djangocms_link.models import Link
 
+
 class LinkPlugin(CMSPluginBase):
 
     model = Link
@@ -29,35 +30,22 @@ class LinkPlugin(CMSPluginBase):
         return context
 
     def get_form(self, request, obj=None, **kwargs):
-        Form = super(LinkPlugin, self).get_form(request, obj, **kwargs)
+        form_class = super(LinkPlugin, self).get_form(request, obj, **kwargs)
 
-        # this is bit tricky, since i don't wont override add_view and 
-        # change_view 
-        class FakeForm(object):
-            def __init__(self, Form, site):
-                self.Form = Form
-                self.site = site
-
-                # base fields are required to be in this fake class, this may
-                # do some troubles, with new versions of django, if there will
-                # be something more required
-                self.base_fields = Form.base_fields
-
-            def __call__(self, *args, **kwargs):
-                # instanciate the form on call
-                form = self.Form(*args, **kwargs)
-                # tell form we are on this site
-                form.for_site(self.site)
-                return form
-        # TODO: Make sure this works
-        if self.cms_plugin_instance.page and self.cms_plugin_instance.page.site:
-            site = self.cms_plugin_instance.page.site
+        if obj and obj.page and obj.page.site:
+            site = obj.page.site
         elif self.page and self.page.site:
             site = self.page.site
         else:
             # this might NOT give the result you expect
             site = Site.objects.get_current()
-        return FakeForm(Form, site)
+
+        class Form(form_class):
+            def __init__(self, *args, **kwargs):
+                super(Form, self).__init__(*args, **kwargs)
+                self.for_site(site)
+
+        return Form
 
     def icon_src(self, instance):
         return settings.STATIC_URL + u"cms/img/icons/plugins/link.png"
