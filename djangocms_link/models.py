53e056ac6e65cd85c2912a9c6ec97df2b4b270e0
Stefan Foulis <stefan@foulis.ch>
2016-02-26 21:20:41 +0100

    fix field clashes with 'file' in Django 1.9
    Set cmsplugin_ptr and remove related_name to avoid field name clashes
    with any other plugin that has a field called "file".
    
    see https://github.com/divio/django-cms/issues/5030

--- djangocms_link/models.py
+++ djangocms_link/models.py
@@ -29,6 +29,12 @@ class AbstractLink(CMSPlugin):
     url_validators = [IntranetURLValidator(intranet_host_re=getattr(
         settings, 'DJANGOCMS_LINK_INTRANET_HOSTNAME_PATTERN', None)), ]
 
+    # Set cmsplugin_ptr and remove related_name to avoid field name clashes
+    # with any other plugin that has a field called "file".
+    # https://github.com/divio/django-cms/issues/5030
+    cmsplugin_ptr = models.OneToOneField(
+        CMSPlugin, related_name='+', parent_link=True)
+
     name = models.CharField(_('name'), max_length=256)
     # Re: max_length, see: http://stackoverflow.com/questions/417142/
     url = models.CharField(_('link'), blank=True, null=True,
