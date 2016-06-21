766699e0f8570cbdcac0a2acb89f71cba9ca398e
Martin Koistinen <mkoistinen@gmail.com>
2016-06-20 23:19:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Environment :: Web Environment',
     'Framework :: Django',
+    'Framework :: Django :: 1.8',
+    'Framework :: Django :: 1.9',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
