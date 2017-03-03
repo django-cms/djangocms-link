0a18556421856dff40ed44cc75133550be2e187d
Paulo Alvarado <commonzenpython@gmail.com>
2017-03-02 20:18:58 -0500

    Fixed #118 -- Use correct field subclass (#119)

--- djangocms_link/fields_select2_legacy.py
+++ djangocms_link/fields_select2_legacy.py
@@ -11,7 +11,13 @@ class Select2LegacyPageSearchField(AutoModelSelect2Field):
     ]
 
     def get_queryset(self):
-        return Page.objects.drafts().on_site(self.site) if self.site is not None else Page.objects.drafts()
+        if self.site:
+            return Page.objects.drafts().on_site(self.site)
+        return Page.objects.drafts()
 
     def security_check(self, request, *args, **kwargs):
-        return request.user and not request.user.is_anonymous() and request.user.is_staff
+        user = request.user
+
+        if user and not user.is_anonymous() and user.is_staff:
+            return True
+        return False
