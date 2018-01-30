23ba87b3da286051f01a2e23571b2b9affa144b2
JensDiemer <git@jensdiemer.de>
2018-01-30 11:20:42 +0100

    update tests to fix #138

--- djangocms_link/fields.py
+++ djangocms_link/fields.py
@@ -5,12 +5,6 @@ from django.conf import settings
 ENABLE_SELECT2 = getattr(settings, 'DJANGOCMS_LINK_USE_SELECT2', False)
 
 if ENABLE_SELECT2 and 'django_select2' in settings.INSTALLED_APPS:
-    import django_select2
-
-    select2_version = LooseVersion(django_select2.__version__)
-    if select2_version >= LooseVersion('5'):
-        from djangocms_link.fields_select2 import Select2PageSearchField as PageSearchField
-    else:
-        from djangocms_link.fields_select2_legacy import Select2LegacyPageSearchField as PageSearchField
+    from djangocms_link.fields_select2 import Select2PageSearchField as PageSearchField
 else:
     from cms.forms.fields import PageSelectFormField as PageSearchField
