cda6ec8f18e4dd9c58a04e071d233c661ee9f02e
Angelo Dini <angelo.dini@divio.ch>
2019-08-15 17:37:28 +0200

    fix tests

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
-from django.conf import settings
 
 from djangocms_link.validators import IntranetURLValidator
+from djangocms_link.models import HOSTNAME
 
 
 class LinkValidatorTestCase(TestCase):
 
     def test_intranet_host_re(self):
-        self.assertIsNone(settings.DJANGOCMS_LINK_INTRANET_HOSTNAME_PATTERN)
-        HOSTNAME = r'[a-z,0-9,-]{1,15}'
+        host = r'[a-z,0-9,-]{1,15}'
         host_re = (
             '(' + IntranetURLValidator.hostname_re
             + IntranetURLValidator.domain_re
             + IntranetURLValidator.tld_re +
-            '|' + HOSTNAME + '|localhost)'
+            '|' + host + '|localhost)'
         )
         validator = IntranetURLValidator(
-            intranet_host_re=HOSTNAME,
+            intranet_host_re=host,
         )
         self.assertEqual(validator.host_re, host_re)
+        self.assertIsNone(HOSTNAME)
