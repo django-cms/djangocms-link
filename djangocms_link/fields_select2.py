fcfc7de33e02db03505460e1669bf9c539fbbe37
Angelo Dini <angelo.dini@divio.ch>
2020-08-27 13:28:20 +0200

    fixes #179 and #180

--- djangocms_link/fields_select2.py
+++ djangocms_link/fields_select2.py
@@ -16,10 +16,23 @@ class Select2PageSearchFieldMixin:
 class Select2PageSelectWidget(Select2PageSearchFieldMixin, ModelSelect2Widget):
     site = None
 
+    # show entries when clicking on it
+    def build_attrs(self, base_attrs, extra_attrs=None):
+        default_attrs = {"data-minimum-input-length": 0}
+        default_attrs.update(base_attrs)
+        attrs = super().build_attrs(default_attrs, extra_attrs=extra_attrs)
+        return attrs
+
     def get_queryset(self):
         if self.site:
             return Page.objects.drafts().on_site(self.site)
         return Page.objects.drafts()
+
+    # we need to implement jQuery ourselves, see #180
+    class Media:
+        js = (
+            "https://code.jquery.com/jquery-3.5.1.slim.min.js",
+        )
 
 
 class Select2PageSearchField(forms.ModelChoiceField):
