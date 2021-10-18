4f60b8a28f7f406ab31d544c17e7ffa2ede31c94
Aiky30 <Aiky30@users.noreply.github.com>
2021-10-18 17:18:05 +0100

    Added missing empty line to djangocms_link/forms.py

--- djangocms_link/forms.py
+++ djangocms_link/forms.py
@@ -40,3 +40,4 @@ class LinkForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['attributes'].widget = AttributesWidget()
+        
