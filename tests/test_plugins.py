d738d3f6761fcf19a99e2f7ffd46a38ed7766335
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2021-11-13 04:26:20 +0000

    Use assertEqual instead of assertEquals for Python 3.11 compatibility.

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -90,7 +90,7 @@ class LinkPluginsTestCase(CMSTestCase):
             warnings.simplefilter("ignore")
             response = self.client.post(request_url, data)
 
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<div class="success">')
 
     def test_optional_link(self):
