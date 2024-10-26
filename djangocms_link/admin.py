ed6fe60453a8f0d94d17d6e95cb832110d0140d7
Fabian Braun <fsbraun@gmx.de>
2024-10-26 14:50:20 +0200

    feat: autodetect linkable models through their admin

--- djangocms_link/admin.py
+++ djangocms_link/admin.py
@@ -1,3 +1,5 @@
+import traceback
+
 from django.apps import apps
 from django.contrib import admin
 from django.core.exceptions import FieldError, PermissionDenied
@@ -6,6 +8,7 @@ from django.urls import path
 from django.views.generic.list import BaseListView
 
 from cms import __version__
+from cms.admin.utils import GrouperModelAdmin
 from cms.models import Page
 from cms.utils import get_language_from_request
 
@@ -17,6 +20,18 @@ if _version >= 4:
     from cms.models import PageContent
 else:
     from cms.models import Title as PageContent
+
+
+REGISTERED_ADMIN = []
+
+for _admin in admin.site._registry.values():
+    if _admin.model._meta.app_label == "cms":
+        continue
+    if getattr(_admin, "search_fields", []) and hasattr(_admin.model, "get_absolute_url"):
+        REGISTERED_ADMIN.append(_admin)
+
+
+print(REGISTERED_ADMIN)
 
 
 class UrlsJsonView(BaseListView):
@@ -44,17 +59,12 @@ class UrlsJsonView(BaseListView):
             raise PermissionDenied
 
         self.object_list = self.get_queryset()
+        self.add_admin_querysets(self.object_list)
         context = self.get_context_data()
+        results = self.get_optgroups(context)
         return JsonResponse(
             {
-                "results": [
-                    {
-                        "text": ("Pages"),
-                        "children": [
-                            self.serialize_result(obj) for obj in context["object_list"]
-                        ],
-                    },
-                ],
+                "results": results,
                 "pagination": {"more": context["page_obj"].has_next()},
             }
         )
@@ -75,47 +85,71 @@ class UrlsJsonView(BaseListView):
         except Exception as e:
             return JsonResponse({"error": str(e)})
 
+    def get_optgroups(self, context):
+        results = []
+        model = {}
+        previous_model = None
+        for obj in context["object_list"]:
+            if obj._meta.verbose_name_plural != previous_model or not model:
+                results.append(model)
+                previous_model = obj._meta.verbose_name_plural
+                model = {
+                    "text": previous_model.capitalize(),
+                    "children": [],
+                }
+            model["children"].append(self.serialize_result(obj))
+        results.append(model)
+        return results
+
     def serialize_result(self, obj):
         """
         Convert the provided model object to a dictionary that is added to the
         results list.
         """
-        return {"id": f"cms.page:{obj.page.pk}", "text": str(obj), "url": obj.get_absolute_url()}
+        return {
+            "id": f"{obj._meta.app_label}.{obj._meta.model_name}:{obj.pk}",
+            "text": str(obj),
+            "url": obj.get_absolute_url()
+        }
 
     def get_queryset(self):
         """Return queryset based on ModelAdmin.get_search_results()."""
-        if _version >= 4:
-            try:
-                # django CMS 4.2+
-                qs = (
-                    PageContent.admin_manager.filter(language=self.language, title__icontains=self.term)
-                    .current_content()
-                    .order_by("page__path")
-                )
-                if self.site:
-                    qs = qs.filter(page__site_id=self.site)
-                qs = list(qs)
-            except FieldError:
-                # django CMS 4.0 - 4.1
-                qs = (
-                    PageContent.admin_manager.filter(language=self.language, title__icontains=self.term)
-                    .current_content()
-                    .order_by("page__node__path")
-                )
-                if self.site:
-                    qs = qs.filter(page__site_id=self.site)
-                qs = list(qs)
-        else:
-            # django CMS 3
-            qs = (PageContent.objects.filter(
+        try:
+            # django CMS 4.2+
+            qs = PageContent.admin_manager.filter(
                 language=self.language, title__icontains=self.term
-            ).order_by("page__node__path"))
+            ).current_content().values_list("page_id", flat=True)
+            qs = Page.objects.filter(pk__in=qs).order_by("path")
             if self.site:
-                qs = qs.filter(page__node_site_id=self.site)
-            qs = list(qs)
-            for page_content in qs:
-                # Patch the missing get_absolute_url method
-                page_content.get_absolute_url = lambda: page_content.page.get_absolute_url()
+                qs = qs.filter(page__site_id=self.site)
+        except FieldError:
+            # django CMS 3.11 - 4.1
+            qs = PageContent.admin_manager.filter(
+                language=self.language, title__icontains=self.term
+            ).current_content().values_list("page_id", flat=True)
+            qs = Page.objects.filter(pk__in=qs).order_by("path")
+            if self.site:
+                qs = qs.filter(page__site_id=self.site)
+        return list(qs)
+
+    def add_admin_querysets(self, qs):
+        for model_admin in REGISTERED_ADMIN:
+            try:
+                # hack: GrouperModelAdmin expects a language to be temporarily set
+                if isinstance(model_admin, GrouperModelAdmin):
+                    model_admin.language = self.language
+                new_qs = model_admin.get_queryset(self.request)
+                new_qs, search_use_distinct = model_admin.get_search_results(
+                    self.request, new_qs, self.term
+                )
+                if search_use_distinct:
+                    new_qs = new_qs.distinct()
+
+                qs += list(new_qs)
+            except Exception as e:
+                print("==>", e)
+                traceback.print_tb(e.__traceback__)
+                # raise e
         return qs
 
     def process_request(self, request):
